4641d8bf15a7b4262004e7cc68b288ed3b00ee9b
Stacie Taylor-Cima <stacietaylor-cima@ip-172-27-241-243.ec2.internal>
2019-09-05 11:08:41 -0700

    Add Cypress to project for end-to-end testing
    I've added Cypress to the project for end-to-end testing because after reading through the intro of their documentation and implementing some tests, I think this is a really great tool for folks who are new to testing.
    
    It allows you to really visualize the tests and helps you generate the syntax you need to create them.
    
    So far, I have only created a test that accesses the create-list page, then tests that when a user clicks the "create shopping list" button, the user is redirected to the list page.

--- cypress/integration/createList.spec.js
+++ cypress/integration/createList.spec.js
@@ -1,25 +1,27 @@
-import { exportAllDeclaration } from "@babel/types";
+import { exportAllDeclaration } from '@babel/types';
 
-describe ('Cyress access test', () => {
-    it ('Visits iNeedToBuy in browser', () => {
-      cy.visit ('/create-list');
-    });
+describe('Cyress access test', () => {
+  it('Visits iNeedToBuy in browser', () => {
+    cy.visit('/create-list');
+  });
+});
+
+describe('Generate list token', function() {
+  it('clicks a button and redirects to list page', function() {
+    // go to the create list page
+    cy.visit('/create-list');
+    // click the create shopping list button
+    cy.get('button').click();
+    // redirect to the list page
+    cy.url().should('include', 'http://localhost:3000/');
   });
 
-  describe('Generate list token', function() {
-    it('clicks a button and redirects to list page', function() {
-        // go to the create list page
-        cy.visit('/create-list');
-        // click the create shopping list button
-        cy.get('button').click()
-        // redirect to the list page
-        cy.url().should('include', 'http://localhost:3000/')
-    })
-
-    it('stores a token value in localStorage', function(){
-        cy.visit('/create-list');
-        cy.get('button').click().should(() => {
-            expect(localStorage.getItem('token')).to.exist
-        })
-    })
+  it('stores a token value in localStorage', function() {
+    cy.visit('/create-list');
+    cy.get('button')
+      .click()
+      .should(() => {
+        expect(localStorage.getItem('token')).to.exist;
+      });
+  });
 });
