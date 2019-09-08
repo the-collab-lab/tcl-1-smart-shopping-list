d0548670efc019f25308e14b60624abf29bdd781
Kate Sowles <katesowles@gmail.com>
2019-09-07 18:18:37 -0700

    route order matters : laughing :

--- src/router.js
+++ src/router.js
@@ -7,12 +7,13 @@ import ItemDetail from './pages/itemDetail';
 
 const Router = () => (
   // NOTE: by not passing a path to the List component, this
-  // becomes the default path for any unrecognized URLs
+  // becomes the default path for any unrecognized URLs.
+  // this default route MUST be at the bottom of the list.
   <Switch>
-    <Route component={List} />
     <Route exact path="/add-item" component={AddItem} />
     <Route exact path="/create-list" component={CreateList} />
     <Route exact path="/item-detail" component={ItemDetail} />
+    <Route component={List} />
   </Switch>
 );
 
