b0b5b0767f7ac90284d228c7497953fad9bba6bc
Kate Sowles <katesowles@gmail.com>
2019-09-07 17:25:08 -0700

    updated router to include a new item-detail page, where we'll have the back button to the list view

--- src/router.js
+++ src/router.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import List from './pages/list';
 import AddItem from './pages/addItem';
-import CreateList from './pages/createList/index';
+import CreateList from './pages/createList';
+import ItemDetail from './pages/itemDetail';
 
-const Router = () => {
-  return (
-    <Switch>
-      <Route exact path="/" component={List} />
-      <Route exact path="/add-item" component={AddItem} />
-      <Route exact path="/create-list" component={CreateList} />
-    </Switch>
-  );
-};
+const Router = () => (
+  // NOTE: by not passing a path to the List component, this
+  // becomes the default path for any unrecognized URLs
+  <Switch>
+    <Route component={List} />
+    <Route exact path="/add-item" component={AddItem} />
+    <Route exact path="/create-list" component={CreateList} />
+    <Route exact path="/item-detail" component={ItemDetail} />
+  </Switch>
+);
 
 export default Router;
