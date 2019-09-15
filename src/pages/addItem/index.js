c106e042472172f0a91875fbf3b2815db1f7364b
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2019-09-15 15:31:46 +0000

    Add radio buttons to add item form
    We've added radio buttons to the form so that the user can set how soon they plan on purchasing that item again. Using React hooks, we've updated the state to include a duration so that when a radio button is selected and the form is submitted, that duration value will be sent to the database.

--- src/pages/addItem/index.js
+++ src/pages/addItem/index.js
@@ -1,29 +1,69 @@
-import React, { useState } from 'react';
-import { withFirestore } from 'react-firestore';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
-import { ContentWrapper, Footer, Header, PageWrapper } from '../../components';
-const AddItem = ({ firestore }) => {
+import { withFirestore } from 'react-firestore';
+import {
+  ContentWrapper,
+  Footer,
+  Header,
+  Loading,
+  PageWrapper,
+} from '../../components';
+import { TokenContext, ListContext } from '../../contexts';
+
+const AddItem = ({ history, firestore }) => {
+  const frequencyOptions = [
+    { display: 'Soon', value: 'soon' },
+    { display: 'Kind of soon', value: 'kind-of-soon' },
+    { display: 'Not Soon', value: 'not-soon' },
+  ];
+
+  // NOTE: the line below is a destructuring declaration, which gives us a more concise way of
+  // grabbing the properties off our context providers, the example here has the same result as
+  // the destructuring syntax:
+  //   const setTokenValue = useContext(TokenContext).setTokenValue;
+  // the value is made more obvious when, in order to grab more than one property, you'd stack
+  // up declarations, like this:
+  //   const token = useContext(TokenContext).token;
+  //   const setTokenValue = useContext(TokenContext).setTokenValue;
+  //   const confirmToken = useContext(TokenContext).confirmToken;
+  const { token } = useContext(TokenContext);
+  const { list, setListValue } = useContext(ListContext);
+
+  // NOTE: setting this particular component's private loading state
+  const [loading, setLoading] = useState(false);
+
+  // NOTE: local state gives the value a place to live before we officially add them to the
+  // app "state" (in the ListContext)
   const [name, setName] = useState('');
-  // Get the list token from localStorage the first time the component renders to avoid making the call to localStorage every time an item is added
-  const [token] = useState(localStorage.getItem('token'));
+  const [frequency, setFrequency] = useState(frequencyOptions[0].value);
 
-  // Send the new item to Firebase
-  const addItem = name => {
-    firestore.collection('items').add({
-      name: name,
-      listToken: token,
-    });
+  // NOTE: users won't have a list to view or add items to if they don't have a token, so
+  // "push" them to where they can get started
+  if (!token) history.push('/create-list');
+
+  const sendNewItemToFirebase = item => {
+    setLoading(true);
+
+    firestore
+      .collection('items')
+      .add(item)
+      .then(response => {
+        const merged = [...list, ...[item]];
+        setListValue(merged);
+        setName('');
+        setFrequency(frequencyOptions[0].value);
+      })
+      .catch(error => console.error('Error getting documents: ', error))
+      .finally(() => setLoading(false));
   };
 
-  // The state every time an event happens
-  const handleChange = event => {
-    setName(event.target.value);
-  };
+  const handleTextChange = event => setName(event.target.value);
 
-  // Handle the click of the Add Item botton on the form
+  const handleRadioButtonChange = event => setFrequency(event.target.value);
+
   const handleSubmit = event => {
     event.preventDefault();
-    addItem(name);
+    sendNewItemToFirebase({ name, frequency, listToken: token });
   };
 
   return (
@@ -31,12 +71,33 @@ const AddItem = ({ firestore }) => {
       <Header />
 
       <ContentWrapper>
-        <form onSubmit={handleSubmit}>
+        {loading && <Loading />}
+        <form className="form-example" onSubmit={handleSubmit}>
           <label>
             Add Item:
-            <input value={name} type="text" id="name" onChange={handleChange} />
+            <input
+              value={name}
+              type="text"
+              id="name"
+              className="name-text-input"
+              onChange={handleTextChange}
+            />
           </label>
-          <button onClick={handleSubmit}>Add item</button>
+
+          {frequencyOptions.map((option, index) => (
+            <label key={'option-' + index}>
+              <input
+                type="radio"
+                name="frequency"
+                value={option.value}
+                className="frequency-radio-button"
+                onChange={handleRadioButtonChange}
+                checked={frequency === option.value}
+              />
+              {option.display}
+            </label>
+          ))}
+          <button type="submit">Add item</button>
         </form>
       </ContentWrapper>
 
@@ -45,10 +106,10 @@ const AddItem = ({ firestore }) => {
   );
 };
 
-// Wrap this component in the higher order componenet withFirestore to directly access the database
 export default withFirestore(AddItem);
 
 AddItem.propTypes = {
-  firestore: PropTypes.any.isRequired,
+  history: PropTypes.object.isRequired,
+  firestore: PropTypes.object.isRequired,
 };
 AddItem.defaultProps = {};
--- src/pages/list/list.test.js
+++ src/pages/list/list.test.js
@@ -5,27 +5,114 @@ import TestRenderer from 'react-test-renderer';
 import { fb as firebase } from '../../lib/firebase.js';
 import { FirestoreProvider } from 'react-firestore';
 import List from './index';
+import { TokenProvider, ListProvider } from './../../contexts';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <FirestoreProvider firebase={firebase}>
-      <BrowserRouter>
-        <List />
-      </BrowserRouter>
-    </FirestoreProvider>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+// NOTE: programatically "pushing" to our app's routing history
+// means have to mock the history object and it's push function
+
+// TODO: mock additional context values and expect the same tests to
+// pass if it CAN see some sort of stored token -- see ListTest for an example
+
+describe('list is only provided list context values, redirects', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <ListProvider value={{ list: [] }}>
+        <FirestoreProvider firebase={firebase}>
+          <BrowserRouter>
+            <List history={{ push: jest.fn() }} />
+          </BrowserRouter>
+        </FirestoreProvider>
+      </ListProvider>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('snapshot on load', () => {
+    const tree = TestRenderer.create(
+      <ListProvider value={{ list: [] }}>
+        <FirestoreProvider firebase={firebase}>
+          <BrowserRouter>
+            <List history={{ push: jest.fn() }} />
+          </BrowserRouter>
+        </FirestoreProvider>
+      </ListProvider>
+    );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 });
 
-it('snapshot on load', () => {
-  const tree = TestRenderer.create(
-    <FirestoreProvider firebase={firebase}>
-      <BrowserRouter>
-        <List />
-      </BrowserRouter>
-    </FirestoreProvider>
-  );
-  expect(tree.toJSON()).toMatchSnapshot();
+describe('list DOES NOT contain items', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <TokenProvider value={{ token: 'abc123' }}>
+        <ListProvider value={{ list: [] }}>
+          <FirestoreProvider firebase={firebase}>
+            <BrowserRouter>
+              <List history={{ push: jest.fn() }} />
+            </BrowserRouter>
+          </FirestoreProvider>
+        </ListProvider>
+      </TokenProvider>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('snapshot on load', () => {
+    const tree = TestRenderer.create(
+      <TokenProvider value={{ token: 'abc123' }}>
+        <ListProvider value={{ list: [] }}>
+          <FirestoreProvider firebase={firebase}>
+            <BrowserRouter>
+              <List history={{ push: jest.fn() }} />
+            </BrowserRouter>
+          </FirestoreProvider>
+        </ListProvider>
+      </TokenProvider>
+    );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
+
+describe('list DOES contain items', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      // <TokenContext.Provider value={{ token: 'abc123' }}>
+      <ListProvider value={{ list: [] }}>
+        <FirestoreProvider firebase={firebase}>
+          <BrowserRouter>
+            <List history={{ push: jest.fn() }} />
+          </BrowserRouter>
+        </FirestoreProvider>
+      </ListProvider>,
+      // </TokenContext.Provider>
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('snapshot on load', () => {
+    const list = [
+      { name: 'one', duration: 'soon', listToken: 'abc123' },
+      { name: 'two', duration: 'soon', listToken: 'abc123' },
+      { name: 'three', duration: 'soon', listToken: 'abc123' },
+    ];
+
+    const tree = TestRenderer.create(
+      // <TokenContext.Provider value={{ token: 'abc123' }}>
+      <ListProvider value={{ list }}>
+        <FirestoreProvider firebase={firebase}>
+          <BrowserRouter>
+            <List history={{ push: jest.fn() }} />
+          </BrowserRouter>
+        </FirestoreProvider>
+      </ListProvider>
+      // </TokenContext.Provider>
+    );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 });
