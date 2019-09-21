6595b5fa99a8f005a80043d77373131a04bec4dd
Caitlyn <cgreffly@gmail.com>
2019-09-21 11:07:46 -0700

    resolved message printed while typing issue, cleaned up code

--- src/pages/addItem/index.js
+++ src/pages/addItem/index.js
@@ -12,17 +12,19 @@ import { TokenContext, ListContext } from '../../contexts';
 
 const AddItem = ({ history, firestore }) => {
   const frequencyOptions = [
-    { display: 'Soon', value: 'soon' },
-    { display: 'Kind of soon', value: 'kind-of-soon' },
-    { display: 'Not Soon', value: 'not-soon' },
+    {
+      display: 'Soon',
+      value: 'soon',
+    },
+    {
+      display: 'Kind of soon',
+      value: 'kind-of-soon',
+    },
+    {
+      display: 'Not Soon',
+      value: 'not-soon',
+    },
   ];
-
-  const { token } = useContext(TokenContext);
-  const { list, setListValue } = useContext(ListContext);
-
-  // NOTE: setting this particular component's private loading state
-  const [loading, setLoading] = useState(false);
-  const [name, setName] = useState('');
 
   // NOTE: the line below is a destructuring declaration, which gives us a more concise way of
   // grabbing the properties off our context providers, the example here has the same result as
@@ -34,31 +36,73 @@ const AddItem = ({ history, firestore }) => {
   //   const setTokenValue = useContext(TokenContext).setTokenValue;
   //   const confirmToken = useContext(TokenContext).confirmToken;
 
+  const { token } = useContext(TokenContext);
+  const { list, setListValue } = useContext(ListContext);
+
+  // NOTE: setting this particular component's private loading state
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState('');
+
   // NOTE: local state gives the value a place to live before we officially add them to the
   // app "state" (in the ListContext)
   const [frequency, setFrequency] = useState(frequencyOptions[0].value);
   const [matchState, setMatchState] = useState(null);
 
+  // SEVEN: no matter if a match is found in ListContext or in the db, we handle it the same...
+  useEffect(() => {
+    if (matchState === false) {
+      // EIGHT: if no match found and not a "null" matchState (aka not ready to be compared because
+      // we're still typing),
+      // NINE: let's build the correct format to send this item to the db!
+
+      const newItem = {
+        name,
+        frequency,
+        listToken: token,
+      };
+      // TEN: once we have the nicely formatted objet, SEND ALL THE THINGS TO FIREBASE! :allthethings:
+      sendNewItemToFirebase(newItem);
+    }
+  }, [matchState]);
+
+  // NOTE: users won't have a list to view or add items to if they don't have a token, so
+  // "push" them to where they can get started
   if (!token) history.push('/create-list');
 
   const checkForDupes = dbList => {
-    return dbList.find(item => item.name.toLowerCase());
+    return dbList.find(item => item.name.toLowerCase() === name.toLowerCase());
   };
 
   const checkItems = () => {
     if (Array.isArray(list) && list.length > 0) {
+      // ONE A: check list context for an array of items
       console.log('in the if ', list);
-      // const dupeIfFound = checkForDupes(list);
-      // console.log('dupeIfFoundFromContext ', dupeIfFound);
-      // dupeIfFound ? setMatchState(true) : setMatchState(false);
+      const dupeIfFound = checkForDupes(list);
+      dupeIfFound ? setMatchState(true) : setMatchState(false);
     } else {
+      // ONE B: if no list context stored, check the db
       console.log('in the else');
       firestore
         .collection('items')
         .where('listToken', '==', token)
         .get()
+        // TWO: TADA! this is how we handle async code! This request to get items with matching listToken
+        // from the db returns a promise, which has three callbacks: then (which is basically "if success", catch
+        // (if an error occured in request), and finally (which will run after EITHER then or catch, can be a way
+        // to wrap things up if the same cleanup steps are required for both success and error results)
         .then(response => {
-          const dupeIfFound = checkForDupes(response.docs);
+          // THREE: normalize this "list", the info coming back from the db comes back with response.doc as a list,
+          // BUT each of the items in the list needs to be "unpacked" by running .data() on it (hence where the
+          // item.data().name stuff came from earlier in our debugging)
+          const normalizedList = response.docs.map(doc => doc.data());
+          // FOUR: once normalized, update the context with the list we just fetched! it'll save us from
+          // having to call it again in a couple steps!
+          setListValue(normalizedList);
+          // FIVE: same as line 63 now that we've tidied up the format of the list coming back from the db to be the same
+          // as the format of our list stored in ListContext
+          const dupeIfFound = checkForDupes(normalizedList);
+          // SIX: same as line 64, which we'll now handle for both the if and else blocks in this function inside of the
+          // useEffect for matchState (above)
           dupeIfFound ? setMatchState(true) : setMatchState(false);
         })
         .catch(function(error) {
@@ -66,15 +110,6 @@ const AddItem = ({ history, firestore }) => {
         });
     }
   };
-  // const addItem = name => {
-  //   firestore.collection('items').add({
-  //     name: name,
-  //     listToken: token,
-  //   });
-  // };
-
-  // NOTE: users won't have a list to view or add items to if they don't have a token, so
-  // "push" them to where they can get started
 
   const sendNewItemToFirebase = item => {
     setLoading(true);
@@ -92,14 +127,6 @@ const AddItem = ({ history, firestore }) => {
       .finally(() => setLoading(false));
   };
 
-  // useEffect(() => {
-  //   if (name.toLowerCase() !== '') {
-  //     checkItems(name.toLowerCase());
-  //   }
-  // }, [name.toLowerCase()]);
-  // const [matchState, setMatchState] = useState(null); // null is neutral "nothing to check state", otherwise pass boolean
-  // const [name, setName] = useState('');
-  // const [token] = useState(localStorage.getItem('token'));
   const handleTextChange = event => {
     setName(event.target.value);
     setMatchState(null);
