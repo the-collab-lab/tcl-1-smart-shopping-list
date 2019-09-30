8ab86346600ac7250fefee36ff9221e8e5637626
Kate Sowles <katesowles@gmail.com>
2019-09-29 19:43:04 -0700

    removing backward compatibility for item frequency

--- src/contexts/list.context.js
+++ src/contexts/list.context.js
@@ -1,6 +1,5 @@
 import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
-import { backwardCompatibleFreqId } from '../lib/frequency';
 
 const ListContext = createContext({
   value: [],
@@ -35,7 +34,7 @@ const ListProvider = ({ children }) => {
   const setListValue = newList => {
     setList(prev => {
       const prevList = { ...prev };
-      return { ...prevList, ...{ list: backwardCompatibleFreqId(newList) } };
+      return { ...prevList, ...{ list: newList } };
     });
   };
 
