804b5a118858dab4905d03122ad75422ee700c3c
itsoliviasparks <itsoliviasparks@gmail.com>
2023-11-19 14:23:44 -0700

    npm audit & adding new database

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,17 +1,16 @@
 // NOTE: import only the Firebase modules that you need in your app... except
 // for the second line, which makes both the linter and react-firebase happy
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 // Initalize Firebase.
 const config = {
-    apiKey: '{process.env.REACT_APP_TCL_1_FIRESTORE_API_KEY}',
-    authDomain: 'tcl-1-i-need-to-buy.firebaseapp.com',
-    databaseURL: 'https://tcl-1-i-need-to-buy.firebaseio.com',
-    projectId: 'tcl-1-i-need-to-buy',
-    storageBucket: 'tcl-1-i-need-to-buy.appspot.com',
-    messagingSenderId: "138816077951",
-    appId: "1:138816077951:web:a45c6b825fc100e8"
+    apiKey: "AIzaSyDvVB6SIAhXqa3chcJgaJi_XGkd1W3xIhI",
+    authDomain: "tcl-1-smart-shopping-list.firebaseapp.com",
+    projectId: "tcl-1-smart-shopping-list",
+    storageBucket: "tcl-1-smart-shopping-list.appspot.com",
+    messagingSenderId: "490626563800",
+    appId: "1:490626563800:web:1c6c138d68d09a6f8de1ad"
 };
 
 let fb = firebase.initializeApp(config);
